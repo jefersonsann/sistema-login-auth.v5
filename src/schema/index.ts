7c85e4d8e751e2aadfc8e8228efe11d135021180
Jeferson Santos <jefersonsann@hotmail.com>
2024-06-06 14:28:05 -0300

    fix: correções

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -4,7 +4,7 @@ export const LoginSchema = z.object({
   usermail: z
     .string()
     .min(1, {
-      message: "Por favor digite um usuario ou e-mail valido.",
+      message: "Por favor digite um usuário ou e-mail valido.",
     })
     .toLowerCase(),
   password: z.string(),
@@ -13,20 +13,20 @@ export const LoginSchema = z.object({
 export const RegisterSchema = z
   .object({
     name: z.string().min(1, {
-      message: "Nome é origatório.",
+      message: "Nome é obrigatório.",
     }),
     username: z
       .string()
-      .min(1, { message: "Nome de usuario é origatório." })
+      .min(1, { message: "Nome de usuário é obrigatório." })
       .toLowerCase(),
     email: z
       .string()
       .min(1, {
-        message: "Por favor digite um usuario ou e-mail valido.",
+        message: "Por favor digite um usuário ou e-mail valido.",
       })
       .toLowerCase(),
     password: z.string().min(5, {
-      message: "A senha deve ter no minimo 5 caracteres.",
+      message: "A senha deve ter no mínimo 5 caracteres.",
     }),
     confirmPassword: z.string(),
   })
